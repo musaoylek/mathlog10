a2fcefd0eb426709b9a0bd811407c007a8544005
musaoylek <101888206+musaoylek@users.noreply.github.com>
2023-07-17 20:07:08 +0300

    log10.js

---
+++ log10.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>musaoylek math.log10 metodu kullanımı</title>
+</head>
+<body>
+	<script>
+		var sayi;
+		sayi= prompt("Lütfen bir sayı giriniz")
+		var ert = Math.log10(sayi);
+		alert("Sayının 10 tabanında değeri : " +ert);
+	</script>
+</body>
+</html>
